fd96559632caf6e88ea00a99fde1e0df374afea1
“EkaterinaSergeeva” <foreversweet@inbox.ru>
2023-03-30 20:30:32 +0500

    add homework 1_1

---
+++ 1_1.py
@@ -0,0 +1,7 @@
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+splitted_line = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'.split(',')
+print(splitted_line)
+print(splitted_line[0])
+print(splitted_line[-1])
+print(splitted_line[1])
+print(splitted_line[-2])
