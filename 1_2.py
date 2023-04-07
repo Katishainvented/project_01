013566e7639b2355065c18d498595bca431a75c0
“EkaterinaSergeeva” <foreversweet@inbox.ru>
2023-04-07 20:14:03 +0500

    add homework 1_2, 1_3

---
+++ 1_2.py
@@ -0,0 +1,31 @@
+# #А
+# my_favorite_songs = [
+#     ['Waste a Moment', 3.03],
+#     ['New Salvation', 4.02],
+#     ['Staying\' Alive', 3.40],
+#     ['Out of Touch', 3.03],
+#     ['A Sorta Fairytale', 5.28],
+#     ['Easy', 4.15],
+#     ['Beautiful Day', 4.04],
+#     ['Nowhere to Run', 2.58],
+#     ['In This World', 4.02],
+# ]
+
+# print(
+#     my_favorite_songs [0][1] + my_favorite_songs[2][1] + my_favorite_songs[7][1]
+# )
+
+
+my_favorite_songs = {
+    'Waste a Moment': 3.03,
+    'New Salvation': 4.02,
+    'Staying\' Alive': 3.40,
+    'Out of Touch': 3.03,
+    'A Sorta Fairytale': 5.28,
+    'Easy': 4.15,
+    'Beautiful Day': 4.04,
+    'Nowhere to Run': 2.58,
+    'In This World': 4.02,
+}
+
+print(my_favorite_songs['New Salvation'] + my_favorite_songs['Easy'] + my_favorite_songs['Nowhere to Run'])
